d11ae3fb975e8c70dc3c63af52beb8cb20c986bf
Jacob Harvey <mccornhusk@gmail.com>
2021-02-25 16:27:55 -0500

    updated Ip address in deploy.js

--- deploy.js
+++ deploy.js
@@ -85,7 +85,7 @@ function sshConnect() {
   ssh
     .connect({
       // TODO: ADD YOUR IP ADDRESS BELOW (e.g. '12.34.5.67')
-      host: '52.90.45.242',
+      host: '35.170.182.97',
       username: 'ubuntu',
       privateKey: 'starter-node.pem'
     })
